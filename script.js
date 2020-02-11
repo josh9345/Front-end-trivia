5bacebd7eed9392f7bf9834b91ecd25d73fe9c02
Joshua Reed <joshualreed93@gmail.com>
2020-02-11 11:47:13 -0500

    user options added

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let answers = []
+
 let a = document.querySelector('#first')
 let b = document.querySelector('#second')
 let c = document.querySelector('#third')
@@ -5,9 +7,9 @@ let d = document.querySelector('#fourth')
 let picture = document.querySelectorAll('.picture')
 
 a.setAttribute('src', 'https://cosmos-images2.imgix.net/file/spina/photo/18032/190218-mount-full.jpg?ixlib=rails-2.1.4&auto=format&ch=Width%2CDPR&fit=max&w=1600')
-// b.setAttribute('src', )
-// c.setAttribute('src', )
-// d.setAttribute('src', )
+b.setAttribute('src', 'https://cosmos-images2.imgix.net/file/spina/photo/18032/190218-mount-full.jpg?ixlib=rails-2.1.4&auto=format&ch=Width%2CDPR&fit=max&w=1600')
+c.setAttribute('src', 'https://cosmos-images2.imgix.net/file/spina/photo/18032/190218-mount-full.jpg?ixlib=rails-2.1.4&auto=format&ch=Width%2CDPR&fit=max&w=1600')
+d.setAttribute('src', 'https://cosmos-images2.imgix.net/file/spina/photo/18032/190218-mount-full.jpg?ixlib=rails-2.1.4&auto=format&ch=Width%2CDPR&fit=max&w=1600')
 
 for(let i=0; i<picture.length; i++){
 	picture[i].addEventListener('click', function(){
