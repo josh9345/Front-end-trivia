10ad3cbfbca7edaad6d4841b3acb7f1e8ffef86f
Joshua Reed <joshualreed93@gmail.com>
2020-02-11 11:28:55 -0500

    event added and test

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-let picture = document.querySelector('.picture')
-picture.setAttribute('src', 'https://cosmos-images2.imgix.net/file/spina/photo/18032/190218-mount-full.jpg?ixlib=rails-2.1.4&auto=format&ch=Width%2CDPR&fit=max&w=1600')
+let a = document.querySelector('#first')
+let b = document.querySelector('#second')
+let c = document.querySelector('#third')
+let d = document.querySelector('#fourth')
+let picture = document.querySelectorAll('.picture')
+
+a.setAttribute('src', 'https://cosmos-images2.imgix.net/file/spina/photo/18032/190218-mount-full.jpg?ixlib=rails-2.1.4&auto=format&ch=Width%2CDPR&fit=max&w=1600')
+// b.setAttribute('src', )
+// c.setAttribute('src', )
+// d.setAttribute('src', )
+
+for(let i=0; i<picture.length; i++){
+	picture[i].addEventListener('click', function(){
+		alert("hello")
+	})
+}
